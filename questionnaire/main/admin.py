433fd1c0cb8d1c8f861120fc171a11d3a6efbcb8
Shandriuk <shandrug.nikita@gmail.com>
2021-06-08 23:25:38 +0300

    adding models to admin

--- questionnaire/main/admin.py
+++ questionnaire/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Questionnaire, Question, Answer
 
 # Register your models here.
+admin.site.register(Questionnaire)
+admin.site.register(Question)
+admin.site.register(Answer)
